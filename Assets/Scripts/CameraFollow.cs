4a6f4541586c0773c636c60ba1d5cd927dcdebf6
Luqmus <rjcnz1007@yandex.ru>
2022-10-25 09:20:46 +0400

    camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,17 +6,10 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform _target;
     [SerializeField] private float _distance;
-    [SerializeField] private float _height;
-
-    // [SerializeField] private float _heightDamping;
 
     private void LateUpdate()
     {
-        float wantedHeight = _target.position.y * _height;
         float currentHeight = transform.position.y;
-
-        // зум на персонажа
-        //currentHeight = Mathf.Lerp(currentHeight, wantedHeight, _heightDamping * Time.deltaTime);
 
         transform.position = _target.position;
         transform.position -= Vector3.forward * _distance;
